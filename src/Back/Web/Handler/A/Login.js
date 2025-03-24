0503a233eca9ef65cc455edf62e14f5f2a082bde
Alex Gusev <alex@flancer64.com>
2025-03-24 19:16:54 +0200

    SVTR-248 Redirect to the Dashboard after login

--- src/Back/Web/Handler/A/Login.js
+++ src/Back/Web/Handler/A/Login.js
@@ -6,14 +6,14 @@ export default class Svelters_Back_Web_Handler_A_Login {
      * @param {typeof import('node:http2')} http2
      * @param {typeof import('node:crypto')} crypto
      * @param {Svelters_Back_Defaults} DEF
-     * @param {TeqFw_Core_Shared_Api_Logger} logger - Logger instance
-     * @param {TeqFw_Web_Back_Help_Respond} respond - Error response helper
-     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper - Database transaction wrapper
+     * @param {TeqFw_Web_Back_Help_Respond} respond
+     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper
      * @param {Fl64_Tmpl_Back_Service_Render} tmplRender
      * @param {Fl64_Auth_Otp_Back_Store_Mem_XsrfToken} memXsrfToken
-     * @param {Fl64_OAuth2_Social_Back_Mod_Provider} modProvider - Module for interacting with OAuth2 providers
+     * @param {Fl64_OAuth2_Social_Back_Mod_Provider} modProvider
      * @param {Fl64_OAuth2_Social_Back_Plugin_Registry_Provider} regProviders
      * @param {Fl64_OAuth2_Social_Back_Store_Mem_State} memState
+     * @param {Fl64_Web_Session_Back_Manager} session
      * @param {Svelters_Back_Web_Handler_A_Z_Helper} zHelper
      * @param {typeof Fl64_Tmpl_Back_Enum_Type} TMPL
      * @param {typeof Fl64_OAuth2_Social_Shared_Enum_Provider_Code} PROVIDER
@@ -23,7 +23,6 @@ export default class Svelters_Back_Web_Handler_A_Login {
             'node:http2': http2,
             'node:crypto': crypto,
             Svelters_Back_Defaults$: DEF,
-            TeqFw_Core_Shared_Api_Logger$$: logger,
             TeqFw_Web_Back_Help_Respond$: respond,
             TeqFw_Db_Back_App_TrxWrapper$: trxWrapper,
             Fl64_Tmpl_Back_Service_Render$: tmplRender,
@@ -31,6 +30,7 @@ export default class Svelters_Back_Web_Handler_A_Login {
             Fl64_OAuth2_Social_Back_Mod_Provider$: modProvider,
             Fl64_OAuth2_Social_Back_Plugin_Registry_Provider$: regProviders,
             Fl64_OAuth2_Social_Back_Store_Mem_State$: memState,
+            Fl64_Web_Session_Back_Manager$: session,
             Svelters_Back_Web_Handler_A_Z_Helper$: zHelper,
             'Fl64_Tmpl_Back_Enum_Type.default': TMPL,
             'Fl64_OAuth2_Social_Shared_Enum_Provider_Code.default': PROVIDER,
@@ -85,6 +85,9 @@ export default class Svelters_Back_Web_Handler_A_Login {
                             .getAuthorizationUrl({clientId: x.clientId, state,});
                 });
 
+                // set the URL to redirect to after authentication
+                const redirectUrl = `/${DEF.SHARED.SPACE}/${DEF.SHARED.ROUTE_DASHBOARD}`;
+                const {headers: headRedirect} = await session.storeRedirectUrl({redirectUrl});
                 // load template and render the page
                 const localeApp = DEF.SHARED.LOCALE;
                 const localeUser = zHelper.getLocale(req);
@@ -94,6 +97,7 @@ export default class Svelters_Back_Web_Handler_A_Login {
                 const {content: body} = await tmplRender.perform({name, type, localeUser, localeApp, view, partials});
                 respond.code200_Ok({
                     res, body, headers: {
+                        ...headRedirect,
                         [HTTP2_HEADER_CONTENT_TYPE]: 'text/html'
                     }
                 });
